88d244c408de0c928c07c49f8622cc2861257466
Tintin2710 <haiding.wang@etu.u-paris.fr>
2024-11-25 13:31:09 +0100

    copy / paste these tests in the test-internal_function.R, will cause error because the backnone is not updated

--- tests/testthat/test-.pack_and_function_check.R
+++ tests/testthat/test-.pack_and_function_check.R
@@ -1,20 +1,41 @@
-test_that(".pack_and_function_check throws error when fun argument does not contain '::'", {
-  # Mock inputs
-  fun <- "geom_point"  # incorrect input
-  
-  # Call the function and expect an error
-  expect_error(.pack_and_function_check(fun, "path/to/library", "fun1"))
+test_that("test .internal_function.R", {
 
-  fun <- "ggplot2::geom_point"
-  lib.path <- "path/to/nonexistent/library"  # incorrect library path
-  
-  # Call the function and expect an error
-  expect_error(.pack_and_function_check(fun, lib.path, "fun1"))
+    # .pack_and_function_check()
+    fun_wrong1 <- "geom_point"  # incorrect input
+    fun_wrong2 <- "ggplot2::non_existent_function"  # incorrect function name
+    fun_good <- "ggplot2::geom_point" # correct input
+    path_wrong <- "path/to/library"  # incorrect input
+    path_good <- NULL  # incorrect input
 
+    testthat::expect_no_error(.pack_and_function_check(
+        fun = fun_good, 
+        lib_path = path_good,
+        external_function_name = "FUN1",
+        external_package_name = "P1"
+    ))
+    testthat::expect_error(.pack_and_function_check(
+        fun = fun_wrong1, 
+        lib_path = path_good,
+        external_function_name = "FUN1",
+        external_package_name = "P1"
+    ))
+    testthat::expect_error(.pack_and_function_check(
+        fun = fun_wrong2, 
+        lib_path = path_good,
+        external_function_name = "FUN1",
+        external_package_name = "P1"
+    ))
+    testthat::expect_error(.pack_and_function_check(
+        fun = fun_good, 
+        lib_path = path_wrong,
+        external_function_name = "FUN1",
+        external_package_name = "P1"
+    ))
 
-  # Mock inputs
-  fun <- "ggplot2::non_existent_function"  # incorrect function name
-  
-  # Call the function and expect an error
-  expect_error(.pack_and_function_check(fun, "path/to/library", "fun1"))
+    # .base_op_check()
+    testthat::expect_no_error(.base_op_check(
+        external_function_name = "FUN1",
+        external_package_name = "P1"
+    ))
+    
 })
